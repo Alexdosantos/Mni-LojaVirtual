2b83ef1a7fe1e70b3be2df00b6b6671965b8d78a
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-05-23 02:29:15 -0300

    atualizado

---
+++ loja-virtual/src/components/Carrinho/Body.Style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const CardProduct = styled.div`
+    display: flex;
+    padding-left:20px ;
+    
+    margin: 0 auto;
+    width: 595px;
+    height: 200px;
+    left: 0px;
+    top: 0px;
+    margin: 0 auto;
+    
+    left: 0px;
+    margin-top: 20px;
+
+
+`
--- loja-virtual/src/components/Carrinho/CarrinhoCompra.jsx
+++ loja-virtual/src/components/Carrinho/CarrinhoCompra.jsx
@@ -5,7 +5,20 @@ import { Infotexth3 } from "../BodyTopo/BodyTop.Style"
 import { Infotexth2 } from "../BodyTopo/BodyTop.Style"
 import { Header } from "../Header/Header"
 import { Footer } from "../Footer/Footer"
+
+import Bf from '../../assets/CapaGames/bf.png'
+import Cod from '../../assets/CapaGames/cod.png'
+import { CardProduct } from "./Body.Style"
+
+
+
 export const CarrinhoCompra = () => {
+  const selectProduct = [
+    {id: 1 , nome:'Battlefield' , price: 'R$10,00' , img: Bf},
+    {id: 2 , nome:'Call of Duty' , price: 'R$10,00' , img: Cod},
+    
+    
+]
   const location = useLocation()
   console.log(location)
   return (
@@ -16,33 +29,38 @@ export const CarrinhoCompra = () => {
         <Infotexth2>Carrinho</Infotexth2>
         <Infotexth3>{2}</Infotexth3>
      </Infotext>
+    {selectProduct.map((produtos) => (
 
-      <div>
-        <div>
-          <img src="" alt="" />
-        </div>
+        <CardProduct>
+            <div>
+              <img src={produtos.img} alt="" />
+            </div>
 
-        <div>
-          <h3>Battlefield</h3>
-          <p>Valor: R$10,00</p>
-          <p>Quantidade:1</p>
+            <div>
+                <h3>Battlefield</h3>
+                <p>Valor: R$10,00</p>
+                <p>Quantidade:1</p>
+            
+                <div>
+                    <button>+</button>
+                    <button>-</button>
+                </div>
+            </div>
+            
+                   
+
+        </CardProduct>
+
+ ))}
+
           <div>
-            <button>+</button>
-            <button>-</button>
+            <p>Total:R$20,00</p>
           </div>
-        </div>
-
-        <div>
-          <p>Total:R$20,00</p>
-        </div>
-        <div>
-          <ButtonDados />
-        </div>
-        <Footer />
-
-      </div>
-
- 
+          <div>
+            <ButtonDados />
+          </div>
+          <Footer />
+      
     
     
     
